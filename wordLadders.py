f2a66e5aa9af67cc0a2982059898efa54f3cb9ee
Natesh Kirpalani <kirfire4@gmail.com>
2017-03-02 04:00:18 +0000

    Added finished wordLadders.py

--- wordLadders.py
+++ wordLadders.py
@@ -2,118 +2,123 @@ from collections import defaultdict
 from collections import Counter
 
 
-def test():
+class Ladder(object):
     
-    track = []
-    sameN = []
- 
-    newL = changeLetter("snakes")
-    newA = changeLetter("brains")
-    
-    word_list1 = []
-    word_list2 = []
-
-    not_found = True
-    
-    
-    
-    
-    f = open('words.txt', 'r')
-    
-    for word in f:
-        if len(word) == 7:
-            sameN.append(word)
-          
-    for q in newL:
-        for a in sameN:
-            if q in a:
-                word_list1.append(q)
-                
-                
-    for m in newA:
-        for n in sameN:
-            if m in n:
-                word_list2.append(m)
-                
-                
-    find_track (word_list1, word_list2, sameN)
-    
-    
-                
-def find_track (start_list, end_list, sameN):
-    
-    y = 0
-    track = []
-    start = start_list
-    end = end_list
-    temp_list = []
-    visited = []
-    
-    found = False
-    
-    while found != True:
+    def __init__(self, file = None):
         
-        for k in start:
+        print "Be patient, this program can take few seconds"
+        
+        self.file = "words.txt"
+        self.track = []
+        self.sameN = []
+        self.visited = []
+        
+        self.queue = []
+    
+        self.not_found = True
+        
+        self.start_list = []
+        self.new_list = []
+        self.target_list =[]
+        self.new_level = []
+         
+        f = open('words.txt', 'r')
+        
+        for word in f:
+            word = word.strip()
+            if len(word) == 6:
+                self.sameN.append(word)
+    
+    
+        #print len(self.sameN)
+        
+        self.word_list1 = self.changeLetter("snakes")
+        self.word_list2 = self.changeLetter("brains")
+        #for r in self.word_list2:
+            #print r
             
-            if found == True:
+            
+            
+            
+    
+    def findtrack(self):
+        
+        #print "in findtrack function"
+        
+        
+        for q in self.word_list1:
+            self.queue.append(q)
+            
+        
+        while self.not_found and len(self.queue) > 0:
+            
+            word_to_check = self.queue.pop(0)
+            
+            #if word_to_check in self.sameN:
+                #print word_to_check
+            self.track.append(word_to_check)
+            
+            #print word_to_check
+                
+            if word_to_check in self.word_list2:
+                self.not_found = False
+                print "Success! This program found the word: "
+                print word_to_check
+                #for p in self.track:
+                    #print p
                 break
-            
-            visited.append(k)
-            temp = changeLetter(k)
-            
-            track.append(k)
-            
-            for h in temp:
-                if found == True:
-                    break
-               
-                if h not in visited:
-                    for a in sameN:
-                        if found == True:
-                            break
-                        if h in a and h not in visited:
-                            visited.append(h)
-                            #print h
-                            temp_list.append(h)
-                            if h in end_list:
-                                print h
-                                found = True
-                                print "success"
-                                #for u in track:
-                                    #print u
-                                break
-                            #else:
-                                #if not track:
-                                    #track.pop()
-            
-            
-        start = temp_list
-        #track.pop()
-
-def changeLetter(base_word):
-    
-    alphabet = ("a", "b", "c", "d", "e","f","g","h","i","j","k","l"
-    ,"m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-    
-    new_word_list = []
-    s = list(base_word)
-    
-    for w in range(0,6):
-        
-        for l in range(26):
-            
-            s[w] = alphabet[l]
-            
-            new_word = "".join(s)
-            
-            new_word_list.append(new_word)
-            
-        s = list(base_word)
-            
-    return new_word_list
                 
+            else:
+                self.check_new_level(word_to_check)
+            
+    
+    
+    def check_new_level (self, upper_level_word):
+        
+        #print "in check new level function"
+        
+        word = upper_level_word
+        new_list = Ladder.changeLetter(self, upper_level_word)
+        
+        for k in new_list:
+            
+            if k not in self.visited:
+                self.visited.append(k)
+                self.queue.append(k)
         
     
+    def changeLetter(self, base_word):
+        
+        #print "in changeLetter function"
+
+        
+        alphabet = ("a", "b", "c", "d", "e","f","g","h","i","j","k","l"
+        ,"m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+        
+        new_word_list = []
+        s = list(base_word)
+        
+        for w in range(0,6):
+            
+            for l in range(26):
+                
+                s[w] = alphabet[l]
+                
+                new_word = "".join(s)
+                
+                if new_word in self.sameN:
+                    new_word_list.append(new_word)
+                
+            s = list(base_word)
+            
+        return new_word_list
 
 
-test()
+def main():
+    print 'Welcome to Word Ladder! Let\'s search through the file to see if there is any words close to "snakes" and "brains" that differ by only one letter!'
+    ladder = Ladder()
+    ladder.findtrack()
+    
+
+if __name__=='__main__':
+    main()
